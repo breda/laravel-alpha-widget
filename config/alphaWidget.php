cbdddb96f19b2b9afbc4e9c7ba33897bbce7f5c9
Reda Bouchaala <bouchaala.reda@gmail.com>
2015-03-26 22:41:00 +0100

    Added spacing to the config file

--- config/alphaWidget.php
+++ config/alphaWidget.php
@@ -8,8 +8,8 @@ return [
 	|--------------------------------------------------------------------------
 	|
 	| This is the Widgets namespace to use when fetching classes,
-   | in order to shorten class names.
-   | This namespace should always, always end with two back-slashs '\\'
+    | in order to shorten class names.
+    | This namespace should always, always end with two back-slashs '\\'
 	|
 	*/
    'namespace' => 'App\Widgets\\',
@@ -20,8 +20,8 @@ return [
 	|--------------------------------------------------------------------------
 	|
 	| This key-value array is represents all the bindings to your widgets
-   | classes. Keys represent widget aliases to work with. Values represent
-   | the class basename that deals with rendring, and gathring data.
+    | classes. Keys represent widget aliases to work with. Values represent
+    | the class basename that deals with rendring, and gathring data.
 	|
 	*/
    'widgets' => [
